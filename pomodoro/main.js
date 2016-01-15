14b593d8409c3c3dfaba87629ac4edd16804e21d
jmchin <justinmichaelchin@gmail.com>
2016-01-15 01:32:38 -0800

    updated main.js

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -1,24 +1,33 @@
+// define global variables
+var breakTime = 5
+var sessionTime = 25
+
+
 // Pomodoro constructor definition
 function Pomodoro() {
     this.state = false;
-    this.breakTime = breakTime;
-    this.sessionTime = sessionTime;
-    // this.time = timeRemaining;
-    // this.id = session;
+    this.breakTime;
+    this.sessionTime;
+    this.time;
+    this.id;
 }
-
-// define variables
-var breakTime = 5
-var sessionTime = 25
 
 // add functionality to Pomodoro prototype
 Pomodoro.prototype = {
     constructor: Pomodoro,
+    startTimer: function() {
+        
+    },
     updateBreak: function() {
-        document.getElementById('breakLng').innerHTML = String(breakTime);
+        document.getElementById('breakLng').textContent = String(breakTime);
+        pomodoro.breakTime = breakTime * 60 * 1000;
     },
     updateSession: function() {
-        document.getElementById('sessionLng').innerHTML = String(sessionTime); 
+        document.getElementById('sessionLng').textContent = String(sessionTime);
+        pomodoro.sessionTime = sessionTime * 60 * 1000;
+    },
+    updateTime: function() {
+        document.getElementById('timerNum').textContent = String(this.time / 1000);
     }
 };
 
@@ -27,17 +36,17 @@ var pomodoro = new Pomodoro();
 
 // event listeners
 document.getElementById('break-down').addEventListener('click', function() {
-  if (breakTime > 0) {
-    breakTime--
-  } else {
-    breakTime === 0
-  }
-  pomodoro.updateBreak()
+    if (breakTime > 0) {
+        breakTime--
+    } else {
+        breakTime === 0
+    }
+    pomodoro.updateBreak()
 })
 
 document.getElementById('break-up').addEventListener('click', function() {
-  breakTime++
-  pomodoro.updateBreak()
+    breakTime++
+    pomodoro.updateBreak()
 })
 
 pomodoro.updateBreak();

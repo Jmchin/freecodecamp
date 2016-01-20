7c88cb7c747153c45cac2ea54cc57428a7ba379e
jmchin <justinmichaelchin@gmail.com>
2016-01-19 20:06:53 -0800

    variable updates removed global variables from main.js and placed that functionality inside of the object properties

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -1,45 +1,41 @@
-// define global variables
-var breakTime = 5
-var sessionTime = 25
-
 // Pomodoro constructor definition
 function Pomodoro() {
-    this.state = false;
-    this.breakTime;
-    this.sessionTime;
-    this.time;
-    this.id;
+  this.state = false;
+  this.breakTime = 5;
+  this.sessionTime = 25;
+  this.time;
+  this.id;
 }
 
 // add functionality to Pomodoro prototype
 Pomodoro.prototype = {
-    constructor: Pomodoro,
-    toggleTimer: function() {
-        if (this.state === false) {
-            this.state = true
-        } else {
-            this.state = false
-        }
-    },
-    updateBreak: function() {
-        document.getElementById('breakLng').textContent = String(breakTime);
-        // convert time to milliseconds
-        pomodoro.breakTime = breakTime * 60 * 1000;
-    },
-    updateSession: function() {
-        document.getElementById('sessionLng').textContent = String(sessionTime);
-        pomodoro.sessionTime = sessionTime * 60 * 1000;
-        pomodoro.updateTime(sessionTime);
-    },
-    updateTime: function(time) {
-        document.getElementById('timerNum').textContent = String(decorateTime(time));
+  toggleTimer: function() {
+    if (this.state === false) {
+      this.state = true
+    } else {
+      this.state = false
     }
-};
-
-function decorateTime(time) {
+  },
+  updateBreak: function() {
+    document.getElementById('breakLng').textContent = String(this.breakTime);
+  },
+  updateSession: function() {
+    document.getElementById('sessionLng').textContent = String(this.sessionTime);
+    this.updateTime(this.sessionTime);
+  },
+  decorateTime: function(time) {
     time = String(time) + ' : 00';
     return time;
-}
+  },
+  updateTime: function(time) {
+    document.getElementById('timerNum').textContent = String(this.decorateTime(time));
+  },
+  initialize: function() {
+    this.updateBreak();
+    this.updateSession();
+    this.updateTime(this.sessionTime);
+  }
+};
 
 // create instance pomodoro of Object Pomodoro
 var pomodoro = new Pomodoro();
@@ -47,41 +43,39 @@ var pomodoro = new Pomodoro();
 // event listeners
 var minusBreak = document.getElementById('break-down');
 minusBreak.addEventListener('click', function() {
-    if (pomodoro.state === false && breakTime > 0) {
-        breakTime--;
-    }
-    pomodoro.updateBreak();
+  if (pomodoro.state === false && pomodoro.breakTime > 0) {
+    pomodoro.breakTime--
+  }
+  pomodoro.updateBreak();
 })
 
 var plusBreak = document.getElementById('break-up');
 plusBreak.addEventListener('click', function() {
-    if (pomodoro.state === false) {
-        breakTime++;
-    }
-    pomodoro.updateBreak();
+  if (pomodoro.state === false) {
+    pomodoro.breakTime++;
+  }
+  pomodoro.updateBreak();
 })
 
 var minusSession = document.getElementById('session-down');
 minusSession.addEventListener('click', function() {
-    if (pomodoro.state === false && sessionTime > 0) {
-        sessionTime--;
-    }
-    pomodoro.updateSession();
+  if (pomodoro.state === false && pomodoro.sessionTime > 0) {
+    pomodoro.sessionTime--;
+  }
+  pomodoro.updateSession();
 })
 
 var plusSession = document.getElementById('session-up');
 plusSession.addEventListener('click', function() {
-    if (pomodoro.state === false) {
-        sessionTime++;
-    }
-    pomodoro.updateSession();
+  if (pomodoro.state === false) {
+    pomodoro.sessionTime++;
+  }
+  pomodoro.updateSession();
 })
 
 var toggleState = document.getElementById('toggle');
 toggleState.addEventListener('click', function() {
-    pomodoro.toggleTimer();
+  pomodoro.toggleTimer();
 })
 
-pomodoro.updateBreak();
-pomodoro.updateSession();
-pomodoro.updateTime(sessionTime);
+pomodoro.initialize();

6a5196e06995f3b44805bd00543fe08bd16a9e29
jmchin <justinmichaelchin@gmail.com>
2016-02-04 23:05:13 -0800

    added evaluate function

--- calculator/main.js
+++ calculator/main.js
@@ -1,5 +1,4 @@
 var display = document.getElementById('num-display');
-var operators = ['-', '+', '*', '/', '=', 'AC'];
 
 // define Calculator object
 function Calculator() {
@@ -9,48 +8,56 @@ function Calculator() {
 
 // extend Calculator functionality
 Calculator.prototype = {
-  updateDisplay: function(value) {
-    if (this.disp === '0') {
-      this.disp = value;
-    } else {
-      this.disp += value;
+    updateDisplay: function(value) {
+      if (this.disp === '0') {
+        this.disp = value;
+      } else {
+        this.disp += value;
+      }
+      display.textContent = this.disp;
+    },
+    clearDisplay: function() {
+      this.disp = '0';
+      this.updateDisplay(this.disp);
+    },
+    delete: function() {
+      this.disp = this.disp.slice(0, -1);
+      display.textContent = display.textContent.slice(0, -1);
+    },
+    evaluate: function() {
+      this.ans = eval(this.disp);
+      // reset disp to 0 after calculation
+      this.disp = '0';
+      display.textContent = this.ans;
     }
-    display.textContent = this.disp;
-  },
-  clearDisplay: function() {
-    this.disp = '0';
-    this.updateDisplay(this.disp);
-  },
-  delete: function() {
-    this.disp = this.disp.slice(0, -1);
-    display.textContent = display.textContent.slice(0, -1);
-  }
 };
+    // instantiate new Calculator
+      var calc = new Calculator();
 
-// instantiate new Calculator
-var calc = new Calculator();
+    /*
+    Event delegate
+    This code exploits the way events 'bubble up'. Instead of setting individual event listeners
+    for all the buttons, we can set a single event listener on the parent element and listen
+    for any events on the children. This is more memory efficient.
+    */
+    document.getElementById('button-tbl').addEventListener('click', function(e) {
+      if (e.target && e.target.nodeName.toLowerCase() === 'button') {
+        var value = e.target.value;
 
-/*
-Event delegate
-This code exploits the way events 'bubble up'. Instead of setting individual event listeners
-for all the buttons, we can set a single event listener on the parent element and listen
-for any events on the children. This is more memory efficient.
-*/
-document.getElementById('button-tbl').addEventListener('click', function(e) {
-  if (e.target && e.target.nodeName.toLowerCase() === 'button') {
-    var value = e.target.value;
+        switch (e.target.value) {
+          case 'AC':
+            calc.clearDisplay();
+            break;
+          case 'DEL':
+            calc.delete();
+            break;
+          case '=':
+            calc.evaluate();
+            break;
+          default:
+            calc.updateDisplay(value);
+        }
+      }
+    });
 
-    switch (e.target.value) {
-      case 'AC':
-        calc.clearDisplay();
-        break;
-      case 'DEL':
-        calc.delete();
-        break;
-      default:
-        calc.updateDisplay(value);
-    }
-  }
-});
-
-calc.updateDisplay(calc.disp);
+    calc.updateDisplay(calc.disp);
